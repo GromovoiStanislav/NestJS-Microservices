5c6d4e9b15310642e22354073a44bca431900112
Gromovoi Stanislav <gromila12@rambler.ru>
2023-08-16 17:50:01 +0600

    Project 14

---
+++ Project 14/Admin/src/product/create-product.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, IsNotEmpty, IsEmpty } from "class-validator";
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+
+  @IsEmpty()
+  likes?: number;
+}
