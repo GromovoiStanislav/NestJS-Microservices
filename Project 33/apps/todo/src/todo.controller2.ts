c2ca4b6ab281a4d0cdef3d1384ad2c7e8335152b
Gromovoi Stanislav <gromila12@rambler.ru>
2024-01-26 16:44:46 +0600

    Project 33

---
+++ Project 33/apps/todo/src/todo.controller2.ts
@@ -0,0 +1,23 @@
+import { Controller } from '@nestjs/common';
+//import { GrpcMethod } from "@nestjs/microservices";
+import { PostTodoDTO, TodoServiceController, TodoServiceControllerMethods } from "proto/todo";
+import { TodoService } from './todo.service';
+
+@Controller()
+@TodoServiceControllerMethods()
+export class TodoController implements TodoServiceController {
+
+  constructor(
+    private readonly todoService: TodoService
+  ) {}
+
+  //@GrpcMethod("TodoService", "PostTodo")
+  async postTodo(postTodoDTO: PostTodoDTO) {
+    return await this.todoService.postTodo(postTodoDTO);
+  }
+
+  //@GrpcMethod("TodoService", "GetTodos")
+  async getTodos() {
+    return await this.todoService.getTodos();
+  }
+}
