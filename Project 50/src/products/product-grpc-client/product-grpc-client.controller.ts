adf82119385f9360ab19b218a7eea33dbe202ef5
Gromovoi Stanislav <gromila12@rambler.ru>
2024-05-04 10:15:33 +0600

    Project 50

--- Project 50/src/products/product-grpc-client/product-grpc-client.controller.ts
+++ Project 50/src/products/product-grpc-client/product-grpc-client.controller.ts
@@ -1,4 +1,4 @@
-import { Get, OnModuleInit } from "@nestjs/common";
+import { Get, OnModuleInit, Param } from "@nestjs/common";
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientGrpc, RpcException } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
@@ -6,19 +6,24 @@ import { Observable } from 'rxjs';
 interface ProductGrpcService {
   create(data: { name: string; price: number }): Observable<any>;
   findAll(data:{}): Observable<any>;
+  findOne(data:{id: number}): Observable<any>;
 }
 
 @Controller('product-grpc-clients')
 export class ProductGrpcClientController implements OnModuleInit {
+
   private productGrpcService: ProductGrpcService;
+
 
   constructor(
     @Inject('PRODUCT_PACKAGE') private client: ClientGrpc) {}
+
 
   onModuleInit() {
     this.productGrpcService =
       this.client.getService<ProductGrpcService>('ProductService');
   }
+
 
   @Post()
   async create(@Body() data) {
@@ -38,4 +43,14 @@ export class ProductGrpcClientController implements OnModuleInit {
       throw new RpcException({ code: e.code, message: e.message });
     }
   }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    try {
+      return this.productGrpcService.findOne({id});
+    } catch (e) {
+      throw new RpcException({ code: e.code, message: e.message });
+    }
+  }
+
 }
