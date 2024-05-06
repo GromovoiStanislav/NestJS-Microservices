8b04f58c7cdf6415e5fdd6e38454002ed541af5f
Gromovoi Stanislav <gromila12@rambler.ru>
2024-05-06 16:16:40 +0600

    Project 50

--- Project 50/src/products/product-grpc-server/product-grpc-server.controller.ts
+++ Project 50/src/products/product-grpc-server/product-grpc-server.controller.ts
@@ -1,52 +1,52 @@
-import { Controller } from '@nestjs/common';
-import { GrpcMethod, RpcException } from '@nestjs/microservices';
+import { Controller } from "@nestjs/common";
+import { GrpcMethod, RpcException } from "@nestjs/microservices";
 
-import { EntityNotFoundError } from 'typeorm';
-import { CreateProductDto } from '../dto/create-product.dto';
-import { ProductsService } from '../products.service';
-import { Metadata, ServerUnaryCall, status }  from "@grpc/grpc-js";
+import { EntityNotFoundError } from "typeorm";
+import { CreateProductDto } from "../dto/create-product.dto";
+import { ProductsService } from "../products.service";
+import { Metadata, ServerUnaryCall, status } from "@grpc/grpc-js";
 
 @Controller()
 export class ProductGrpcServerController {
 
   constructor(
     private productsService: ProductsService
-  ) {}
+  ) {
+  }
 
-  @GrpcMethod('ProductService')
+  @GrpcMethod("ProductService")
   create(
     data: CreateProductDto,
     metadata: Metadata,
-    // call: ServerUnaryCall<CreateProductDto>,
+    call: ServerUnaryCall<CreateProductDto, any>
   ) {
     console.log(data, metadata);
     return this.productsService.create(data);
   }
 
-  // @GrpcMethod('ProductService')
-  // update(
-  //   data: { id: number; name: string; price: number },
-  //   metadata: Metadata,
-  //   call: ServerUnaryCall<CreateProductDto>,
-  // ) {
-  //   //console.log(data, metadata, call);
-  //   const { id, ...rest } = data;
-  //   return this.productsService.update(id, rest);
-  // }
+  @GrpcMethod("ProductService")
+  update(
+    data: { id: number; name: string; price: number },
+    metadata: Metadata,
+    call: ServerUnaryCall<CreateProductDto, any>
+  ) {
+    const { id, ...rest } = data;
+    return this.productsService.update(id, rest);
+  }
 
-  @GrpcMethod('ProductService')
+  @GrpcMethod("ProductService")
   findOne(data: { id: number }) {
     const { id } = data;
     return this.productsService.findOne(id);
   }
 
-  @GrpcMethod('ProductService')
+  @GrpcMethod("ProductService")
   async findAll() {
     const products = await this.productsService.findAll();
     return { data: products };
   }
 
-  @GrpcMethod('ProductService', 'Delete')
+  @GrpcMethod("ProductService", "Delete")
   async remove(data: { id: number }) {
     const { id } = data;
     //return await this.productsService.remove(id);
@@ -56,8 +56,8 @@ export class ProductGrpcServerController {
     } catch (e) {
       if (e instanceof EntityNotFoundError) {
         throw new RpcException({
-          message: 'Product not found',
-          code: status.NOT_FOUND,
+          message: "Product not found",
+          code: status.NOT_FOUND
         });
       }
     }
